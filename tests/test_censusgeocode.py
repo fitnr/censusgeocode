a1814b3b3cc71895d82801ab389e5c461c679cf5
fitnr <fitnr@fakeisthenewreal>
2015-07-24 18:31:34 -0400

    extend tests a whole bunch
    100% coverage

--- tests/test_censusgeocode.py
+++ tests/test_censusgeocode.py
@@ -20,12 +20,38 @@ class CensusGeoCodeTestCase(unittest.TestCase):
     def setUp(self):
         self.cg = CensusGeocode(benchmark='Public_AR_ACS2014', geovintage='ACS2013')
 
+        self.results = self.cg.coordinates(-74, 43)
+
     def test_returns(self):
-        results = self.cg.coordinates(-74, 43)
-        assert isinstance(results, CensusResult)
+        assert isinstance(self.results, CensusResult)
+
+    def test_input(self):
+        assert self.results.input
 
     def test_coords(self):
-        results = self.cg.coordinates(-74, 43)
-        assert results.geographies['Counties'][0]['BASENAME'] == 'Saratoga'
-        assert results.geographies['Counties'][0]['GEOID'] == '36091'
-        assert results.geographies['Census Tracts'][0]['BASENAME'] == "615"
+        assert self.results[0]['Counties'][0]['BASENAME'] == 'Saratoga'
+        assert self.results[0]['Counties'][0]['GEOID'] == '36091'
+        assert self.results[0]['Census Tracts'][0]['BASENAME'] == "615"
+
+    def test_address(self):
+        results = self.cg.address('1600 Pennsylvania Avenue NW', city='Washington', state='DC', zipcode='20500')
+        assert results[0]
+        assert results[0]['geographies']['Counties'][0]['BASENAME'] == 'District of Columbia'
+
+    def test_onelineaddress(self):
+        results = self.cg.onelineaddress('1600 Pennsylvania Avenue NW, Washington, DC, 20500', layers='all')
+        assert results[0]
+        assert results[0]['geographies']['Counties'][0]['BASENAME'] == 'District of Columbia'
+
+        assert 'Metropolitan Divisions' in results[0]['geographies'].keys()
+        assert 'Alaska Native Village Statistical Areas' in results[0]['geographies'].keys()
+
+    def test_address_return_type(self):
+        results = self.cg.address('1600 Pennsylvania Avenue NW', city='Washington', state='DC', zipcode='20500', returntype='locations')
+
+        assert results[0]['matchedAddress'].upper() == '1600 PENNSYLVANIA AVE NW, WASHINGTON, DC, 20502'
+        assert results[0]['addressComponents']['streetName'] == 'PENNSYLVANIA'
+
+    def test_addressbatch(self):
+        with self.assertRaises(NotImplementedError):
+            self.cg.addressbatch(None)
