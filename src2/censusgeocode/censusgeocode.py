7818da3b217a6e687aaba6ac4713e416877ae03a
fitnr <fitnr@fakeisthenewreal>
2015-10-04 14:32:11 -0400

    add license to readme, elsewhere

--- src2/censusgeocode/censusgeocode.py
+++ src2/censusgeocode/censusgeocode.py
@@ -1,5 +1,20 @@
 #!/usr/local/bin/python2
 
+# Copyright (C) 2015 Neil Freeman
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Census Geocoder wrapper
 see http://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.pdf
