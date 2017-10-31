3029e47ffb60ba9f559c0241092f1792157c3b62
fitnr <fitnr@fakeisthenewreal>
2017-10-31 12:57:44 -0400

    remove python3.3 support

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Operating System :: OS Independent',
     ],
