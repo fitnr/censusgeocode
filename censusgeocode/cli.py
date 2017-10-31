3af6d2cc2928a827aeb0289954afc2acf64194ca
fitnr <fitnr@fakeisthenewreal>
2017-10-31 13:53:00 -0400

    add version & timeout flags to censusgeocode cli

--- censusgeocode/cli.py
+++ censusgeocode/cli.py
@@ -13,20 +13,23 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+from __future__ import print_function
 import sys
 import argparse
 from .censusgeocode import CensusGeocode
+from . import __version__
 
 def main():
     parser = argparse.ArgumentParser('censusgeocode')
 
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s v' + __version__)
     parser.add_argument('address', type=str)
+    parser.add_argument('--timeout', metavar='SECONDS', type=int, default=12, help='Request timeout [default: 12]')
 
     args = parser.parse_args()
 
     gc = CensusGeocode()
-    result = gc.onelineaddress(args.address)
+    result = gc.onelineaddress(args.address, timeout=args.timeout)
 
     try:
         print('{}, {}'.format(result[0]['coordinates']['x'], result[0]['coordinates']['y']))
